7f6d893975299eca3ecb360e3acef31bd24125df
simone-nolberto <simone.nolberto@outlook.com>
2024-02-28 16:53:13 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,26 +7,62 @@ const maxCellsNumb = 100;
 
 const markup = `<div class="box"></div>`;
 
-const button = document.querySelector('btn-play');
+let markupContent = 0;
 
+const button = document.querySelector('.btn-play');
+button.addEventListener("click", createGrid)
 
 function createGrid() {
 
-    for (let i = 0; i < maxCellsNumb; i++) {
+    for (let i = 0; i <= maxCellsNumb; i++) {
 
         container.insertAdjacentHTML('beforeend', markup);
 
     }
 
+    const boxElements = document.getElementsByClassName('box');
+    // console.log(boxElements);
 
+    for (let i = 0; i < boxElements.length; i++) {
+        const element = boxElements[i];
+
+        element.addEventListener('click', function (e) {
+
+            element.classList.toggle('bg');
+
+        })
+
+    };
 
     return container
 
 }
 
-button.addEventListener("click", createGrid)
+// const numbArray = [];
+
+// for (let i = 0; i < maxCellsNumb; i++) {
+
+//     function getRndInteger(min, max) {
+//         return Math.floor((max - min) ) + min;
+//       }
+
+//     const randomNumber = Math.floor(Math.random() * 100) +1;
+//     console.log(randomNumber);
+//     numbArray.push(randomNumber);
+
+// }
+
+// console.log(getRndInteger(1, 100));
 
 
-// button.addEventListener('click', createGrid ());
+let numberArray = [];
 
 
+for (let i = 0; i <= 100; i++){
+    
+    numberArray.push(i++)
+
+    
+}
+
+console.log(numberArray);
