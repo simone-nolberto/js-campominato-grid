6b7950dd5ba98d6b2e0de5510b22e2783e63f640
simone-nolberto <simone.nolberto@outlook.com>
2024-03-05 17:00:04 +0100

    update
    working remove event listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const container = document.getElementById('container');
 
 const maxCellsNumb = 100;
 
-const markup = `<div class="box df"></div>`;
+//const markup = `<div class="box df"></div>`;
 
 const resultsOutput = document.querySelector('.results-box');
 
@@ -35,15 +35,61 @@ function generateMushroom() {
 
 }
 
+function modifyCells(e) {
+
+    const element = e.target;
+    console.log(element)
+    console.log('ciaonone')
+    const cellNumber = Number(e.target.getAttribute('cellNumber'));
+    console.log(cellNumber)
+
+    if (mushroomArray.includes(cellNumber + 1)) {
+        element.classList.add('red');
+        element.innerHTML = "🍄";
+        loserCell.push(i + 1);
+        console.log(loserCell);
+
+    } else {
+        element.classList.add('clear');
+        clearCellsClicked.push(i + 1,)
+        // console.log(clearCellsClicked);
+    }
+    console.log(loserCell.length);
+
+    if (clearCellsClicked.length == (maxCellsNumb - mushroomArray.length)) {
+        resultsOutput.innerHTML = "Complimenti, hai vinto!"
+
+    } else if (loserCell.length == 1) {
+        document.querySelector('.results-box').innerHTML = `Hai trovato ${clearCellsClicked.length} celle valide, ma hai perso. Clicca su "Gioca" per riprovare!`;
+
+        let boxElements = document.getElementsByClassName('box');
+        // console.log(boxElements);
+
+
+        for (let i = 0; i < boxElements.length; i++) {
+            const boxElement = boxElements[i];
+
+            boxElement.removeEventListener('click', modifyCells)
+
+        }
+
+    }
+
+    return container;
+};
+
+
 function createGrid() {
 
     mushroomArray = generateMushroom();
+    console.log(mushroomArray);
     clearCellsClicked = [];
     loserCell = [];
     container.innerHTML = '';
     resultsOutput.innerHTML = '';
 
     for (let i = 0; i < maxCellsNumb; i++) {
+        const markup = `<div class="box df" cellNumber="${i}"></div>`;
         container.insertAdjacentHTML('beforeend', markup)
     }
 
@@ -51,46 +97,19 @@ function createGrid() {
     let boxElements = document.getElementsByClassName('box');
     // console.log(boxElements);
 
+
     for (let i = 0; i < boxElements.length; i++) {
         const element = boxElements[i];
         element.innerHTML = i + 1;
-        // console.log(element.innerHTML);
+        //  console.log(element.innerHTML);
+        
+        element.addEventListener('click', modifyCells)
+    }
+
+};
+
+// return container
 
 
-        element.addEventListener('click', function (e) {
-
-            if (mushroomArray.includes(i + 1)) {
-                element.classList.add('red');
-                element.innerHTML = "🍄";
-                loserCell.push(i + 1);
-                console.log(loserCell);
-
-            } else {
-                element.classList.add('clear');
-                clearCellsClicked.push(i + 1,)
-                // console.log(clearCellsClicked);
-            }
-
-            console.log(loserCell.length);
-
-
-            if (clearCellsClicked.length == (maxCellsNumb - mushroomArray.length)) {
-                resultsOutput.innerHTML = "Complimenti, hai vinto!"
-
-            } else if (loserCell.length == 1) {
-                document.querySelector('.results-box').innerHTML = `Hai trovato ${clearCellsClicked.length} celle valide, ma hai perso. Clicca su "Gioca" per riprovare!`;
-                
-                element.removeEventListener('click', function () {
-
-                })
-            }
-
-        })
-
-    };
-
-    return container
-
-}
 
 
